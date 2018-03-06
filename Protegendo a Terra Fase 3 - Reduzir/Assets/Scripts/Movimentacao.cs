b81496296bb8ace8c375536adf44cde10ab5ccc5
Unknown <igorjordany@gmail.com>
2018-03-06 11:32:34 -0400

    Contrução Cenário
    Cenário com paredes e chão finalizado

---
+++ Protegendo a Terra Fase 3 - Reduzir/Assets/Scripts/Movimentacao.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentacao : MonoBehaviour
+{
+
+    public float VelocidadeDeMovimento = 3;
+    public float VelocidadeDeRotacao = 120;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float Horizontal = Input.GetAxis("Horizontal") * VelocidadeDeRotacao * Time.deltaTime;
+        transform.Rotate(0, Horizontal, 0);
+        float Vertical = Input.GetAxis("Vertical") * VelocidadeDeMovimento * Time.deltaTime;
+        transform.Translate(0, 0, Vertical);
+    }
+}
