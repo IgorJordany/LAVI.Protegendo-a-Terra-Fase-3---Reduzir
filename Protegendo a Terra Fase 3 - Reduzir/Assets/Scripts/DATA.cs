b406262621b52c734ec48aa77d6de8eb01154eb8
Unknown <igorjordany@gmail.com>
2018-03-26 20:44:42 -0400

    Teste de menu de seleção personagem

---
+++ Protegendo a Terra Fase 3 - Reduzir/Assets/Scripts/DATA.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DATA : MonoBehaviour {
+    public GameObject[] Datas;
+    public static int PersonagemAInstanciar;
+
+    private void Awake()
+    {
+        Datas = GameObject.FindGameObjectsWithTag("DATA");
+        if(Datas.Length >= 2)
+        {
+            Destroy(Datas[0]);
+        }
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
---
+++ Protegendo a Terra Fase 3 - Reduzir/Assets/Scripts/PlayerSelect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour {
+    public Texture[] Personagem;
+    public int SelecaoAtual;
+
+    private void Start()
+    {
+        SelecaoAtual = 0;
+    }
+    void OnGUI()
+    {
+        if(SelecaoAtual == 0)
+        {
+            GUI.DrawTexture(new Rect(Screen.width / 2 - Screen.width / 4, Screen.height / 2 - Screen.height / 2.2f, Screen.width / 2, Screen.height / 1.2f), Personagem[SelecaoAtual]);
+            if (GUI.Button(new Rect(Screen.width / 1.3f, Screen.height / 2.2f, Screen.width / 6.5f, Screen.height / 5), "PROXIMO"))
+            {
+                SelecaoAtual = SelecaoAtual + 1;
+            }
+        }
+        if(SelecaoAtual > 0 && SelecaoAtual <(Personagem.Length - 1))
+        {
+            GUI.DrawTexture(new Rect(Screen.width / 2 - Screen.width / 4, Screen.height / 2 - Screen.height / 2.2f, Screen.width / 2, Screen.height / 1.2f), Personagem[SelecaoAtual]);
+            if (GUI.Button(new Rect(Screen.width / 1.3f, Screen.height / 2.2f, Screen.width / 6.5f, Screen.height / 5), "PROXIMO"))
+            {
+                SelecaoAtual = SelecaoAtual + 1;
+            }
+        }
+        if(SelecaoAtual >= (Personagem.Length - 1))
+        {
+            GUI.DrawTexture(new Rect(Screen.width / 2 - Screen.width / 4, Screen.height / 2 - Screen.height / 2.2f, Screen.width / 2, Screen.height / 1.2f), Personagem[SelecaoAtual]);
+            if (GUI.Button(new Rect(Screen.width / 1.3f, Screen.height / 2.2f, Screen.width / 6.5f, Screen.height / 5), "PROXIMO"))
+            {
+                SelecaoAtual = SelecaoAtual + 1;
+            }
+        }
+    }
+}
