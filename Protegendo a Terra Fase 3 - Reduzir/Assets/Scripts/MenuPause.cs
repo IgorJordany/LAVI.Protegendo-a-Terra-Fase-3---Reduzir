bbef9374b051297718e207175b4b195944ae06a8
Unknown <igorjordany@gmail.com>
2018-04-05 13:17:07 -0400

    Menu com botões tops

---
+++ Protegendo a Terra Fase 3 - Reduzir/Assets/Scripts/MenuPause.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour {
+    private GameObject player;
+    public void voltar()
+    {
+        player = GameObject.Find("PersonagensM");
+        Destroy(player);
+        player = GameObject.Find("PersonagensF");
+        Destroy(player);
+        SceneManager.LoadScene("Menu");
+    }
+}
