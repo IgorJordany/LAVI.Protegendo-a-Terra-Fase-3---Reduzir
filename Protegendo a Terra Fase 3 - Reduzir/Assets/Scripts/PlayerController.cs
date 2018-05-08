cdcf94dd796462726501447ac4fbe734a8e2ccae
Unknown <igorjordany@gmail.com>
2018-05-08 11:21:15 -0400

    Animação dos personagens

--- Protegendo a Terra Fase 3 - Reduzir/Assets/Scripts/PlayerController.cs
+++ Protegendo a Terra Fase 3 - Reduzir/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,12 @@ public class PlayerController : MonoBehaviour
     //private float VelocidadeDeMovimento = 10;
     public Joystick joystick;
     public Rigidbody rb;
+    public Animator ControladorDeAnimacao;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        ControladorDeAnimacao = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,11 +23,18 @@ public class PlayerController : MonoBehaviour
         joystick = FindObjectOfType<Joystick>();
         //rb.velocity = new Vector3(- (joystick.Horizontal * 20f), 0, -(joystick.Vertical * 20f));
         rb.transform.Translate(joystick.Horizontal, 0, joystick.Vertical);
+        IniciaAnimacao(joystick.Horizontal, joystick.Vertical);
         /*
         float Horizontal = Input.GetAxis("Horizontal") * VelocidadeDeMovimento * Time.deltaTime;
         transform.Translate(Horizontal, 0, 0);
         float Vertical = Input.GetAxis("Vertical") * VelocidadeDeMovimento * Time.deltaTime;
         transform.Translate(0, 0, Vertical);
         */
+    }
+    private void IniciaAnimacao(float horizontal, float vertical)
+    {
+        bool walking = horizontal != 0 || vertical != 0;
+
+        ControladorDeAnimacao.SetBool("andando", walking);
     }
 }
